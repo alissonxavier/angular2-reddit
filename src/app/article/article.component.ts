76b08ea56fe0d5a0ed44f4cc48012e5d8d7f230a
Alisson Xavier de Albuquerque <alisson.albuquerque.ifactory@caixaseguros.com.br>
2017-05-31 15:19:33 -0300

    Inicio dos fontes

---
+++ src/app/article/article.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Article } from './article.model';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css']
+})
+export class ArticleComponent implements OnInit {
+  @Input() article : Article;
+
+   voteUp(){
+     this.article.voteUp();
+     return false;
+   }
+
+   voteDown(){
+     this.article.voteDown();
+     return false;
+   }
+
+  ngOnInit() {
+  }
+
+}
